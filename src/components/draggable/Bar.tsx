aaace0cf54ecde032c171b2c3c5f1e2bfc3bfff6
Lee Janzen-Morel <leejanzen91@gmail.com>
2024-08-13 13:38:40 -0700

    fixed the bar to rendering closable menus

--- src/components/draggable/Bar.tsx
+++ src/components/draggable/Bar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDrag, useDrop } from "react-dnd";
 import { FaBars } from "react-icons/fa";
+import * as Ariakit from "@ariakit/react";
 import styles from "./Draggable.module.scss";
 
 export interface DraggableItem {
@@ -38,20 +39,29 @@ const Bar: React.FC<BarProps> = ({ item, index, moveBar, addChild }) => {
       <div className={styles.handle}>
         <FaBars className={styles.hamburgerIcon} />
       </div>
-      <div className={styles.titleContainer}>{item.title}</div>
-      {item.children && (
-        <div className={styles.nestedContainer}>
-          {item.children.map((child, childIndex) => (
-            <Bar
-              key={child.id}
-              item={child}
-              index={childIndex}
-              moveBar={moveBar}
-              addChild={addChild}
-            />
-          ))}
-        </div>
-      )}
+      <div className={styles.titleContainer}>
+        <Ariakit.DisclosureProvider>
+          <Ariakit.Disclosure className={styles.disclosureButton}>
+            {item.title}
+          </Ariakit.Disclosure>
+          <Ariakit.DisclosureContent className={styles.disclosureContent}>
+            {item.content}
+            {item.children && (
+              <div className={styles.nestedContainer}>
+                {item.children.map((child, childIndex) => (
+                  <Bar
+                    key={child.id}
+                    item={child}
+                    index={childIndex}
+                    moveBar={moveBar}
+                    addChild={addChild}
+                  />
+                ))}
+              </div>
+            )}
+          </Ariakit.DisclosureContent>
+        </Ariakit.DisclosureProvider>
+      </div>
     </div>
   );
 };
