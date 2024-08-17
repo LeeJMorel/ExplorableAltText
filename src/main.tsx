0a548a7398c176a82d1abe20ef6117810375bf3c
Lee <leejanzen91@gmail.com>
2024-08-17 11:02:00 -0700

    corrects routes to not disrupt draggable

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import App from "./App";
 import "./index.scss";
 
@@ -13,7 +11,5 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <DndProvider backend={HTML5Backend}>
-    <RouterProvider router={router} />
-  </DndProvider>
+  <RouterProvider router={router} />
 );
